fcfb701c3e1813a3f545c3739a4ada5053aa3d6b
TheDarkBrotherhood <bassawdavid@gmail.com>
2017-05-03 19:41:00 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ var inGameName = "Theophilus";
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-//var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-//var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "";
