401ef2bb0240efcb3dfc74c62a807419dd98e89e
TheDarkBrotherhood <bassawdavid@gmail.com>
2017-05-03 19:32:39 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+﻿// ***********************************************************************************
+// ID of the Google spreadsheet
+// ***********************************************************************************
+// Make sure your spreadsheet is published (File > Publish to the web…)
+var spreadsheetId = "1bDTmSL4JoflgoIRdaCjnV-oCN2A4v3fdJ2x4pkABkHY";
+
+// ***********************************************************************************
+// Your FC and IGN
+// ***********************************************************************************
+var friendCode = "0232-7801-3192";
+var inGameName = "Theophilus";
+
+// ***********************************************************************************
+// URL of your Reddit/Twitter account
+// ***********************************************************************************
+//var contactUrl = "http://reddit.com/u/richi3f";
+
+// ***********************************************************************************
+// URL of your PGL trainer icon (optional)
+// ***********************************************************************************
+// Download it from the Pokémon Global Link and re-upload to an image-sharing website
+//var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
